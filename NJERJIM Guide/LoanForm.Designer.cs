ab7198b471c536caded525628fa424ae8cb77f9f
Jeremy Bacuta <jbrynnbacuta@gmail.com>
2022-05-24 23:46:53 +0800

    loan module - create loan module

---
+++ NJERJIM Guide/LoanForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace NJERJIM_Guide
+{
+    partial class Loan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backButton = new System.Windows.Forms.Button();
+            this.selectedIdLabel = new System.Windows.Forms.Label();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.dateTimeTextBox = new System.Windows.Forms.TextBox();
+            this.DateTimeLabel = new System.Windows.Forms.Label();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.amountLabel = new System.Windows.Forms.Label();
+            this.addLoanButton = new System.Windows.Forms.Button();
+            this.clientIdTextBox = new System.Windows.Forms.TextBox();
+            this.clientIdLabel = new System.Windows.Forms.Label();
+            this.loanDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.loanDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.backButton.Location = new System.Drawing.Point(911, 489);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 34;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // selectedIdLabel
+            // 
+            this.selectedIdLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectedIdLabel.AutoSize = true;
+            this.selectedIdLabel.Location = new System.Drawing.Point(706, 12);
+            this.selectedIdLabel.Name = "selectedIdLabel";
+            this.selectedIdLabel.Size = new System.Drawing.Size(65, 15);
+            this.selectedIdLabel.TabIndex = 33;
+            this.selectedIdLabel.Text = "Selected ID";
+            this.selectedIdLabel.Visible = false;
+            // 
+            // idLabel
+            // 
+            this.idLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(575, 12);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(17, 15);
+            this.idLabel.TabIndex = 32;
+            this.idLabel.Text = "Id";
+            this.idLabel.Visible = false;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteButton.Location = new System.Drawing.Point(911, 129);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 31;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // dateTimeTextBox
+            // 
+            this.dateTimeTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimeTextBox.Location = new System.Drawing.Point(706, 100);
+            this.dateTimeTextBox.Name = "dateTimeTextBox";
+            this.dateTimeTextBox.Size = new System.Drawing.Size(280, 23);
+            this.dateTimeTextBox.TabIndex = 24;
+            // 
+            // DateTimeLabel
+            // 
+            this.DateTimeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DateTimeLabel.AutoSize = true;
+            this.DateTimeLabel.Location = new System.Drawing.Point(575, 103);
+            this.DateTimeLabel.Name = "DateTimeLabel";
+            this.DateTimeLabel.Size = new System.Drawing.Size(60, 15);
+            this.DateTimeLabel.TabIndex = 23;
+            this.DateTimeLabel.Text = "Date Time";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.amountTextBox.Location = new System.Drawing.Point(706, 71);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(280, 23);
+            this.amountTextBox.TabIndex = 22;
+            // 
+            // amountLabel
+            // 
+            this.amountLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.amountLabel.AutoSize = true;
+            this.amountLabel.Location = new System.Drawing.Point(575, 74);
+            this.amountLabel.Name = "amountLabel";
+            this.amountLabel.Size = new System.Drawing.Size(51, 15);
+            this.amountLabel.TabIndex = 21;
+            this.amountLabel.Text = "Amount";
+            // 
+            // addLoanButton
+            // 
+            this.addLoanButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.addLoanButton.Location = new System.Drawing.Point(830, 129);
+            this.addLoanButton.Name = "addLoanButton";
+            this.addLoanButton.Size = new System.Drawing.Size(75, 23);
+            this.addLoanButton.TabIndex = 20;
+            this.addLoanButton.Text = "Add";
+            this.addLoanButton.UseVisualStyleBackColor = true;
+            this.addLoanButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // clientIdTextBox
+            // 
+            this.clientIdTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clientIdTextBox.Location = new System.Drawing.Point(706, 42);
+            this.clientIdTextBox.Name = "clientIdTextBox";
+            this.clientIdTextBox.Size = new System.Drawing.Size(280, 23);
+            this.clientIdTextBox.TabIndex = 19;
+            // 
+            // clientIdLabel
+            // 
+            this.clientIdLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clientIdLabel.AutoSize = true;
+            this.clientIdLabel.Location = new System.Drawing.Point(575, 45);
+            this.clientIdLabel.Name = "clientIdLabel";
+            this.clientIdLabel.Size = new System.Drawing.Size(52, 15);
+            this.clientIdLabel.TabIndex = 18;
+            this.clientIdLabel.Text = "Client ID";
+            // 
+            // loanDataGridView
+            // 
+            this.loanDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loanDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.loanDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.loanDataGridView.Name = "loanDataGridView";
+            this.loanDataGridView.RowTemplate.Height = 25;
+            this.loanDataGridView.Size = new System.Drawing.Size(544, 500);
+            this.loanDataGridView.TabIndex = 17;
+            this.loanDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.loanDataGridView_CellClick);
+            // 
+            // Loan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 524);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.selectedIdLabel);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.dateTimeTextBox);
+            this.Controls.Add(this.DateTimeLabel);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.amountLabel);
+            this.Controls.Add(this.addLoanButton);
+            this.Controls.Add(this.clientIdTextBox);
+            this.Controls.Add(this.clientIdLabel);
+            this.Controls.Add(this.loanDataGridView);
+            this.Name = "Loan";
+            this.Text = "LoanForm";
+            ((System.ComponentModel.ISupportInitialize)(this.loanDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Label selectedIdLabel;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox dateTimeTextBox;
+        private System.Windows.Forms.Label DateTimeLabel;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Label amountLabel;
+        private System.Windows.Forms.Button addLoanButton;
+        private System.Windows.Forms.TextBox clientIdTextBox;
+        private System.Windows.Forms.Label clientIdLabel;
+        private System.Windows.Forms.DataGridView loanDataGridView;
+    }
+}
---
+++ NJERJIM Guide/LoanForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NJERJIM_Guide
+{
+    public partial class Loan : Form
+    {
+        public Loan()
+        {
+            InitializeComponent(); 
+            InitializeData();
+        }
+        private void InitializeData()
+        {
+            SetDGV();
+        }
+
+        private void SetDGV()
+        {
+            var db_helper = new DatabaseHelper();
+            var query = "select * from loan";
+            db_helper.SetDataGridView(loanDataGridView, query);
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            Program.MainMenuForm.Show();
+            this.Close();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var db_helper = new DatabaseHelper();
+            db_helper.Manipulate($"INSERT INTO {DTLoan.TableName} ({DTLoan.ClientId}, {DTLoan.Amount}, {DTLoan.DateTime}) " +
+                $"VALUES('{clientIdTextBox.Text}', '{amountTextBox.Text}', '{dateTimeTextBox.Text}');");
+            SetDGV();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            var db_helper = new DatabaseHelper();
+            Trace.WriteLine($"DELETE FROM {DTLoan.TableName} WHERE {DTLoan.Id}={selectedIdLabel.Text};");
+            db_helper.Manipulate($"DELETE FROM {DTLoan.TableName} WHERE {DTLoan.Id}={selectedIdLabel.Text};");
+            SetDGV();
+        }
+
+        private void loanDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var selectedRow = loanDataGridView.Rows[e.RowIndex].Cells;
+
+            selectedIdLabel.Text = selectedRow[0].Value.ToString();
+            clientIdTextBox.Text = selectedRow[1].Value.ToString();
+            amountTextBox.Text = selectedRow[2].Value.ToString();
+            dateTimeTextBox.Text = selectedRow[3].Value.ToString();
+
+            idLabel.Visible = true;
+            selectedIdLabel.Visible = true;
+        }
+    }
+}
