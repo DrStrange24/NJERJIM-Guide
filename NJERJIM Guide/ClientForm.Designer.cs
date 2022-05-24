25a4a5ace5371d21b0efe15d5327c1866aa16aa7
Jeremy Bacuta <jbrynnbacuta@gmail.com>
2022-05-24 22:16:37 +0800

    make the add button function

--- NJERJIM Guide/ClientForm.Designer.cs
+++ NJERJIM Guide/ClientForm.Designer.cs
@@ -30,6 +30,20 @@ namespace NJERJIM_Guide
         private void InitializeComponent()
         {
             this.clientDataGridView = new System.Windows.Forms.DataGridView();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.addClientButton = new System.Windows.Forms.Button();
+            this.middleNameTextBox = new System.Windows.Forms.TextBox();
+            this.middleNameLabel = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.sexTextBox = new System.Windows.Forms.TextBox();
+            this.sexLabel = new System.Windows.Forms.Label();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.contactNumberTextBox = new System.Windows.Forms.TextBox();
+            this.contactNumberLabel = new System.Windows.Forms.Label();
+            this.deleteButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.clientDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,25 +55,208 @@ namespace NJERJIM_Guide
             this.clientDataGridView.Location = new System.Drawing.Point(12, 12);
             this.clientDataGridView.Name = "clientDataGridView";
             this.clientDataGridView.RowTemplate.Height = 25;
-            this.clientDataGridView.Size = new System.Drawing.Size(554, 656);
+            this.clientDataGridView.Size = new System.Drawing.Size(554, 419);
             this.clientDataGridView.TabIndex = 0;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(625, 86);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(64, 15);
+            this.firstNameLabel.TabIndex = 1;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.firstNameTextBox.Location = new System.Drawing.Point(756, 83);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(240, 23);
+            this.firstNameTextBox.TabIndex = 2;
+            // 
+            // addClientButton
+            // 
+            this.addClientButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.addClientButton.Location = new System.Drawing.Point(840, 408);
+            this.addClientButton.Name = "addClientButton";
+            this.addClientButton.Size = new System.Drawing.Size(75, 23);
+            this.addClientButton.TabIndex = 3;
+            this.addClientButton.Text = "Add";
+            this.addClientButton.UseVisualStyleBackColor = true;
+            this.addClientButton.Click += new System.EventHandler(this.addClientButton_Click);
+            // 
+            // middleNameTextBox
+            // 
+            this.middleNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.middleNameTextBox.Location = new System.Drawing.Point(756, 112);
+            this.middleNameTextBox.Name = "middleNameTextBox";
+            this.middleNameTextBox.Size = new System.Drawing.Size(240, 23);
+            this.middleNameTextBox.TabIndex = 5;
+            // 
+            // middleNameLabel
+            // 
+            this.middleNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.middleNameLabel.AutoSize = true;
+            this.middleNameLabel.Location = new System.Drawing.Point(625, 115);
+            this.middleNameLabel.Name = "middleNameLabel";
+            this.middleNameLabel.Size = new System.Drawing.Size(79, 15);
+            this.middleNameLabel.TabIndex = 4;
+            this.middleNameLabel.Text = "Middle Name";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lastNameTextBox.Location = new System.Drawing.Point(756, 141);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(240, 23);
+            this.lastNameTextBox.TabIndex = 7;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(625, 144);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(63, 15);
+            this.lastNameLabel.TabIndex = 6;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // sexTextBox
+            // 
+            this.sexTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sexTextBox.Location = new System.Drawing.Point(756, 170);
+            this.sexTextBox.Name = "sexTextBox";
+            this.sexTextBox.Size = new System.Drawing.Size(240, 23);
+            this.sexTextBox.TabIndex = 9;
+            // 
+            // sexLabel
+            // 
+            this.sexLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sexLabel.AutoSize = true;
+            this.sexLabel.Location = new System.Drawing.Point(625, 173);
+            this.sexLabel.Name = "sexLabel";
+            this.sexLabel.Size = new System.Drawing.Size(25, 15);
+            this.sexLabel.TabIndex = 8;
+            this.sexLabel.Text = "Sex";
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addressTextBox.Location = new System.Drawing.Point(756, 228);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(240, 23);
+            this.addressTextBox.TabIndex = 13;
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(625, 231);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(49, 15);
+            this.addressLabel.TabIndex = 12;
+            this.addressLabel.Text = "Address";
+            // 
+            // contactNumberTextBox
+            // 
+            this.contactNumberTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.contactNumberTextBox.Location = new System.Drawing.Point(756, 199);
+            this.contactNumberTextBox.Name = "contactNumberTextBox";
+            this.contactNumberTextBox.Size = new System.Drawing.Size(240, 23);
+            this.contactNumberTextBox.TabIndex = 11;
+            // 
+            // contactNumberLabel
+            // 
+            this.contactNumberLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.contactNumberLabel.AutoSize = true;
+            this.contactNumberLabel.Location = new System.Drawing.Point(625, 202);
+            this.contactNumberLabel.Name = "contactNumberLabel";
+            this.contactNumberLabel.Size = new System.Drawing.Size(96, 15);
+            this.contactNumberLabel.TabIndex = 10;
+            this.contactNumberLabel.Text = "Contact Number";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteButton.Location = new System.Drawing.Point(921, 408);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 14;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
             // 
             // Client
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(954, 680);
+            this.ClientSize = new System.Drawing.Size(1008, 443);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.contactNumberTextBox);
+            this.Controls.Add(this.contactNumberLabel);
+            this.Controls.Add(this.sexTextBox);
+            this.Controls.Add(this.sexLabel);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.middleNameTextBox);
+            this.Controls.Add(this.middleNameLabel);
+            this.Controls.Add(this.addClientButton);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.firstNameLabel);
             this.Controls.Add(this.clientDataGridView);
             this.Name = "Client";
             this.Text = "Client";
             ((System.ComponentModel.ISupportInitialize)(this.clientDataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView clientDataGridView;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Button addClientButton;
+        private System.Windows.Forms.TextBox middleNameTextBox;
+        private System.Windows.Forms.Label middleNameLabel;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox sexTextBox;
+        private System.Windows.Forms.Label sexLabel;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.TextBox contactNumberTextBox;
+        private System.Windows.Forms.Label contactNumberLabel;
+        private System.Windows.Forms.Button deleteButton;
     }
 }
 
--- NJERJIM Guide/ClientForm.cs
+++ NJERJIM Guide/ClientForm.cs
@@ -23,5 +23,18 @@ namespace NJERJIM_Guide
             var query = "select * from client";
             db_helper.SetDataGridView(clientDataGridView, query);
         }
+
+        private void addClientButton_Click(object sender, EventArgs e)
+        {
+            var db_helper = new DatabaseHelper();
+            db_helper.Manipulate($"INSERT INTO {DTClient.TableName} ({DTClient.FirstName}, {DTClient.MiddleName}, {DTClient.LastName}, {DTClient.Sex}, {DTClient.ContactNumber}, {DTClient.Addess}) " +
+                $"VALUES('{firstNameTextBox.Text}', '{middleNameTextBox.Text}', '{lastNameTextBox.Text}', '{sexTextBox.Text}', '{contactNumberTextBox.Text}', '{addressTextBox.Text}');");
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            //var db_helper = new DatabaseHelper();
+            //db_helper.Manipulate($"DELETE FROM {DTClient.TableName} WHERE {DTClient.Id}={0};");
+        }
     }
 }
