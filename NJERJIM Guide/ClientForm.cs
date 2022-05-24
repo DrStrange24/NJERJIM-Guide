7c6465c139d22e8d4dcfd44acc8dbab0029b4ed7
Jeremy Bacuta <jbrynnbacuta@gmail.com>
2022-05-24 23:20:42 +0800

    client module - automatically close after clicking back button

--- NJERJIM Guide/ClientForm.cs
+++ NJERJIM Guide/ClientForm.cs
@@ -63,6 +63,7 @@ namespace NJERJIM_Guide
         private void backButton_Click(object sender, EventArgs e)
         {
             new MainMenu().Show();
+            this.Close();
         }
     }
 }
