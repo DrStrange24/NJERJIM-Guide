a4b523593baec62dd237c1218be8941de8e5fbde
Jeremy Bacuta <jbrynnbacuta@gmail.com>
2022-05-31 19:34:34 +0800

    collection module - data filter features

--- NJERJIM Guide/Apps/CollectionForm.Designer.cs
+++ NJERJIM Guide/Apps/CollectionForm.Designer.cs
@@ -46,6 +46,16 @@ namespace NJERJIM_Guide
             this.loanLabel = new System.Windows.Forms.Label();
             this.remarksLabel = new System.Windows.Forms.Label();
             this.remarksRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchByComboBox = new System.Windows.Forms.ComboBox();
+            this.fromDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.toDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dateTimeToLabel = new System.Windows.Forms.Label();
+            this.dateTimeFromLabel = new System.Windows.Forms.Label();
+            this.searchByLabel = new System.Windows.Forms.Label();
+            this.dateTimeCheckBox = new System.Windows.Forms.CheckBox();
+            this.loanIdComboBox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.collectionDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -105,15 +115,15 @@ namespace NJERJIM_Guide
             this.collectionDataGridView.Name = "collectionDataGridView";
             this.collectionDataGridView.ReadOnly = true;
             this.collectionDataGridView.RowTemplate.Height = 25;
-            this.collectionDataGridView.Size = new System.Drawing.Size(544, 500);
+            this.collectionDataGridView.Size = new System.Drawing.Size(544, 387);
             this.collectionDataGridView.TabIndex = 35;
             this.collectionDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.collectionDataGridView_CellClick);
             // 
             // totalCollectionLabel
             // 
-            this.totalCollectionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalCollectionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.totalCollectionLabel.AutoSize = true;
-            this.totalCollectionLabel.Location = new System.Drawing.Point(575, 493);
+            this.totalCollectionLabel.Location = new System.Drawing.Point(12, 414);
             this.totalCollectionLabel.Name = "totalCollectionLabel";
             this.totalCollectionLabel.Size = new System.Drawing.Size(92, 15);
             this.totalCollectionLabel.TabIndex = 47;
@@ -121,9 +131,9 @@ namespace NJERJIM_Guide
             // 
             // totalCollectionValueLabel
             // 
-            this.totalCollectionValueLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalCollectionValueLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.totalCollectionValueLabel.AutoSize = true;
-            this.totalCollectionValueLabel.Location = new System.Drawing.Point(706, 493);
+            this.totalCollectionValueLabel.Location = new System.Drawing.Point(143, 414);
             this.totalCollectionValueLabel.Name = "totalCollectionValueLabel";
             this.totalCollectionValueLabel.Size = new System.Drawing.Size(0, 15);
             this.totalCollectionValueLabel.TabIndex = 48;
@@ -226,11 +236,126 @@ namespace NJERJIM_Guide
             this.remarksRichTextBox.TabIndex = 64;
             this.remarksRichTextBox.Text = "";
             // 
+            // searchLabel
+            // 
+            this.searchLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Location = new System.Drawing.Point(12, 493);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(42, 15);
+            this.searchLabel.TabIndex = 66;
+            this.searchLabel.Text = "Search";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.searchTextBox.Location = new System.Drawing.Point(213, 489);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(231, 23);
+            this.searchTextBox.TabIndex = 67;
+            this.searchTextBox.TextChanged += new System.EventHandler(this.searchByTextBox_TextChanged);
+            // 
+            // searchByComboBox
+            // 
+            this.searchByComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.searchByComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.searchByComboBox.FormattingEnabled = true;
+            this.searchByComboBox.Items.AddRange(new object[] {
+            "Loan ID",
+            "Client Name"});
+            this.searchByComboBox.Location = new System.Drawing.Point(86, 489);
+            this.searchByComboBox.Name = "searchByComboBox";
+            this.searchByComboBox.Size = new System.Drawing.Size(121, 23);
+            this.searchByComboBox.TabIndex = 68;
+            this.searchByComboBox.TextChanged += new System.EventHandler(this.searchByComboBox_TextChanged);
+            // 
+            // fromDateTimePicker
+            // 
+            this.fromDateTimePicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.fromDateTimePicker.Location = new System.Drawing.Point(142, 449);
+            this.fromDateTimePicker.Name = "fromDateTimePicker";
+            this.fromDateTimePicker.Size = new System.Drawing.Size(200, 23);
+            this.fromDateTimePicker.TabIndex = 70;
+            this.fromDateTimePicker.ValueChanged += new System.EventHandler(this.fromtoDateTimePicker_ValueChanged);
+            // 
+            // toDateTimePicker
+            // 
+            this.toDateTimePicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.toDateTimePicker.Location = new System.Drawing.Point(373, 449);
+            this.toDateTimePicker.Name = "toDateTimePicker";
+            this.toDateTimePicker.Size = new System.Drawing.Size(200, 23);
+            this.toDateTimePicker.TabIndex = 72;
+            this.toDateTimePicker.ValueChanged += new System.EventHandler(this.fromtoDateTimePicker_ValueChanged);
+            // 
+            // dateTimeToLabel
+            // 
+            this.dateTimeToLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dateTimeToLabel.AutoSize = true;
+            this.dateTimeToLabel.Location = new System.Drawing.Point(348, 455);
+            this.dateTimeToLabel.Name = "dateTimeToLabel";
+            this.dateTimeToLabel.Size = new System.Drawing.Size(19, 15);
+            this.dateTimeToLabel.TabIndex = 71;
+            this.dateTimeToLabel.Text = "To";
+            // 
+            // dateTimeFromLabel
+            // 
+            this.dateTimeFromLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dateTimeFromLabel.AutoSize = true;
+            this.dateTimeFromLabel.Location = new System.Drawing.Point(101, 455);
+            this.dateTimeFromLabel.Name = "dateTimeFromLabel";
+            this.dateTimeFromLabel.Size = new System.Drawing.Size(35, 15);
+            this.dateTimeFromLabel.TabIndex = 73;
+            this.dateTimeFromLabel.Text = "From";
+            // 
+            // searchByLabel
+            // 
+            this.searchByLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.searchByLabel.AutoSize = true;
+            this.searchByLabel.Location = new System.Drawing.Point(60, 493);
+            this.searchByLabel.Name = "searchByLabel";
+            this.searchByLabel.Size = new System.Drawing.Size(20, 15);
+            this.searchByLabel.TabIndex = 74;
+            this.searchByLabel.Text = "By";
+            // 
+            // dateTimeCheckBox
+            // 
+            this.dateTimeCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dateTimeCheckBox.AutoSize = true;
+            this.dateTimeCheckBox.Location = new System.Drawing.Point(12, 453);
+            this.dateTimeCheckBox.Name = "dateTimeCheckBox";
+            this.dateTimeCheckBox.Size = new System.Drawing.Size(76, 19);
+            this.dateTimeCheckBox.TabIndex = 75;
+            this.dateTimeCheckBox.Text = "DateTime";
+            this.dateTimeCheckBox.UseVisualStyleBackColor = true;
+            this.dateTimeCheckBox.CheckedChanged += new System.EventHandler(this.dateTimeCheckBox_CheckedChanged);
+            // 
+            // loanIdComboBox
+            // 
+            this.loanIdComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.loanIdComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.loanIdComboBox.FormattingEnabled = true;
+            this.loanIdComboBox.Location = new System.Drawing.Point(213, 489);
+            this.loanIdComboBox.Name = "loanIdComboBox";
+            this.loanIdComboBox.Size = new System.Drawing.Size(121, 23);
+            this.loanIdComboBox.TabIndex = 76;
+            this.loanIdComboBox.Visible = false;
+            this.loanIdComboBox.SelectedIndexChanged += new System.EventHandler(this.loanIdComboBox_SelectedIndexChanged);
+            // 
             // CollectionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(998, 524);
+            this.Controls.Add(this.loanIdComboBox);
+            this.Controls.Add(this.dateTimeCheckBox);
+            this.Controls.Add(this.searchByLabel);
+            this.Controls.Add(this.dateTimeFromLabel);
+            this.Controls.Add(this.toDateTimePicker);
+            this.Controls.Add(this.dateTimeToLabel);
+            this.Controls.Add(this.fromDateTimePicker);
+            this.Controls.Add(this.searchByComboBox);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.searchLabel);
             this.Controls.Add(this.remarksLabel);
             this.Controls.Add(this.remarksRichTextBox);
             this.Controls.Add(this.clearInputsButton);
@@ -276,5 +401,15 @@ namespace NJERJIM_Guide
         private System.Windows.Forms.Label loanLabel;
         private System.Windows.Forms.Label remarksLabel;
         private System.Windows.Forms.RichTextBox remarksRichTextBox;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.ComboBox searchByComboBox;
+        private System.Windows.Forms.DateTimePicker fromDateTimePicker;
+        private System.Windows.Forms.DateTimePicker toDateTimePicker;
+        private System.Windows.Forms.Label dateTimeToLabel;
+        private System.Windows.Forms.Label dateTimeFromLabel;
+        private System.Windows.Forms.Label searchByLabel;
+        private System.Windows.Forms.CheckBox dateTimeCheckBox;
+        private System.Windows.Forms.ComboBox loanIdComboBox;
     }
 }
--- NJERJIM Guide/Apps/CollectionForm.cs
+++ NJERJIM Guide/Apps/CollectionForm.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,7 @@ namespace NJERJIM_Guide
 {
     public partial class CollectionForm : Form
     {
+        private DatabaseHelper db_query = new DatabaseHelper();
         public CollectionForm()
         {
             InitializeComponent();
@@ -24,21 +26,29 @@ namespace NJERJIM_Guide
         {
             void SetDataGridView()
             {
-                var db_helper = new DatabaseHelper();
-                db_helper.SetDataGridView(collectionDataGridView, $"select {DTCollection.Id} as [ID],{DTCollection.LoanId} as [Loan ID],{DTClient.FirstName} as [First Name]," +
-                    $"{DTCollection.Amount} as [Amount],{DTCollection.DateTime} as [Date],{DTCollection.Remarks} as [Remarks] from {DTCollection.Table} join {DTLoan.Table} on {DTCollection.LoanId}={DTLoan.Id} " +
-                    $"join {DTClient.Table} on {DTLoan.ClientId}={DTClient.Id} order by {DTCollection.DateTime} desc;");
+                void SetSearchLoanIdComboBox()
+                {
+                    var data = db_query.GetData($"select {DTLoan.Id} from {DTLoan.Table}");
+                    for (int i = 0; i < data.Rows.Count; i++)
+                        loanIdComboBox.Items.Add(data.Rows[i][0]);
+                    loanIdComboBox.SelectedIndex = 0;
+                }
+
+                searchByComboBox.SelectedIndex = 1;
+                fromDateTimePicker.Enabled = false;
+                toDateTimePicker.Enabled = false;
+                fromDateTimePicker.Value = DateTime.Now.Date;
+                toDateTimePicker.Value = DateTime.Now.Date.AddDays(1).AddMilliseconds(-1);
+                SetSearchLoanIdComboBox();
             }
             void SetTotalCollection()
             {
-                var db_helper = new DatabaseHelper();
-                var data = db_helper.GetData($"select * from {DTCollection.Table}");
+                var data = db_query.GetData($"select * from {DTCollection.Table}");
                 totalCollectionValueLabel.Text = CurrencyFormat.ToString(DSCollection.TotalAmount(DSCollection.GetList(data)));
             }
             void SetComboBoxItems()
             {
-                var db_helper = new DatabaseHelper();
-                var data = db_helper.GetData($"select {DTLoan.Id},{DTLoan.Amount},{DTClient.FirstName} from {DTLoan.Table} join {DTClient.Table} on {DTLoan.ClientId}={DTClient.Id};");
+                var data = db_query.GetData($"select {DTLoan.Id},{DTLoan.Amount},{DTClient.FirstName} from {DTLoan.Table} join {DTClient.Table} on {DTLoan.ClientId}={DTClient.Id};");
                 loanComboBox.Items.Clear();
                 for (int i = 0; i < data.Rows.Count; i++)
                 {
@@ -53,6 +63,47 @@ namespace NJERJIM_Guide
             SetDataGridView();
             SetTotalCollection();
             SetComboBoxItems();
+        }
+
+        private void FilterDataGridView()
+        {
+            string SearchFilter()
+            {
+                string condition = string.Empty;
+                switch(searchByComboBox.SelectedItem.ToString())
+                {
+                    case "Loan ID":
+                        condition = $"{DTLoan.Id}={loanIdComboBox.SelectedItem}";
+                        return condition;
+                    case "Client Name":
+                        condition = $"{DTClient.FirstName} like '%{searchTextBox.Text}%'";
+                        return condition;
+                }
+                return condition;
+            }
+            string DateFilter()
+            {
+                string dateRange = string.Empty;
+                if (dateTimeCheckBox.Checked)
+                {
+                    dateRange += " and ";
+                    dateRange += $"{DTCollection.DateTime} between '{DatabaseHelper.DateTimeToString(fromDateTimePicker.Value)}' and '{DatabaseHelper.DateTimeToString(toDateTimePicker.Value)}'";
+                    return dateRange;
+                }
+                return dateRange;
+            }
+            void SetTotalCollection()
+            {
+                double amount = 0;
+                for(int i=0;i< collectionDataGridView.Rows.Count; i++)
+                    amount +=Convert.ToDouble(collectionDataGridView.Rows[i].Cells["Amount"].Value);
+                totalCollectionValueLabel.Text = CurrencyFormat.ToString(amount);
+            }
+            db_query.SetDataGridView(collectionDataGridView, $"select {DTCollection.Id} as [ID],{DTCollection.LoanId} as [Loan ID],{DTClient.FirstName} as [First Name]," +
+                    $"{DTCollection.Amount} as [Amount],{DTCollection.DateTime} as [Date],{DTCollection.Remarks} as [Remarks] from {DTCollection.Table} join {DTLoan.Table} on {DTCollection.LoanId}={DTLoan.Id} " +
+                    $"join {DTClient.Table} on {DTLoan.ClientId}={DTClient.Id} where {SearchFilter()} {DateFilter()} order by {DTCollection.DateTime} desc;");
+            SetTotalCollection();
+
         }
 
         private void backButton_Click(object sender, EventArgs e)
@@ -126,6 +177,61 @@ namespace NJERJIM_Guide
         private void creatCollectionTextBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter) createLoanButton_Click(null, null);
+        }
+        private void searchByTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if(searchTextBox.Visible) FilterDataGridView();
+        }
+
+        private void dateTimeCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!dateTimeCheckBox.Checked)
+            {
+                fromDateTimePicker.Value = DateTime.Now.Date;
+                toDateTimePicker.Value = DateTime.Now.Date.AddDays(1).AddMilliseconds(-1);
+                fromDateTimePicker.Enabled = false;
+                toDateTimePicker.Enabled = false;
+            }
+            else
+            {
+                fromDateTimePicker.Enabled = true;
+                toDateTimePicker.Enabled = true;
+            }
+            FilterDataGridView();
+        }
+
+        private void fromtoDateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            FilterDataGridView();
+        }
+
+        private void loanIdComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(loanIdComboBox.Visible) FilterDataGridView();
+        }
+
+        private void searchByComboBox_TextChanged(object sender, EventArgs e)
+        {
+            try 
+            {
+                switch (searchByComboBox.SelectedItem.ToString())
+                {
+                    case "Loan ID":
+                        searchTextBox.Visible = false;
+                        searchTextBox.Text = string.Empty;
+                        loanIdComboBox.Visible = true;
+                        loanIdComboBox.SelectedIndex = 0;
+                        break;
+                    case "Client Name":
+                        searchTextBox.Visible = true;
+                        searchTextBox.Text = string.Empty;
+                        loanIdComboBox.Visible = false;
+                        loanIdComboBox.SelectedIndex = 0;
+                        break;
+                }
+                FilterDataGridView();
+            }
+            catch { }
         }
     }
 }
